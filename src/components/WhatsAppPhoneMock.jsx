785c260d74f0a4f8b9bff3450aefef2d333cb75c
josemaria.torres <josemaria.torres@greta.pe>
2025-10-18 09:46:54 -0500

    18/10

--- src/components/WhatsAppPhoneMock.jsx
+++ src/components/WhatsAppPhoneMock.jsx
@@ -4,14 +4,12 @@ import whatsappPhoneImage from '../assets/Celular.svg';
 const WhatsAppPhoneMock = () => {
     return (
         <div className="flex items-center justify-center w-full h-full">
-            <div className="w-80 h-78 flex items-center justify-center">
-                <img
+                 <img
                     src={whatsappPhoneImage}
                     alt="WhatsApp Mock"
                     className="w-80 h-auto max-h-96 object-contain"
                 />
-            </div>
-        </div>
+         </div>
     );
 };
 
